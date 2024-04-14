17f6aa8639e820029949ca1501d225c388b92b45
Sunil-B-K <sunilkuligod21@gmail.com>
2024-04-14 15:57:23 +0530

    mulet

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://e-backend-jkn8.onrender.com/images/${req.file.filename}`,
   });
 });
 
@@ -56,10 +56,6 @@ const Product = mongoose.model("Product", {
     require: true,
   },
   name: {
-    type: String,
-    require: true,
-  },
-  image: {
     type: String,
     require: true,
   },
